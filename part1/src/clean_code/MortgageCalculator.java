ab8114bfd3a6e586c2a258e3cfc4980c2c8f16fc
prajul-shinde <prajulshinde@gmail.com>
2023-10-23 12:06:01 -0700

    added monthly payment calulator to project

--- part1/src/clean_code/MortgageCalculator.java
+++ part1/src/clean_code/MortgageCalculator.java
@@ -9,31 +9,40 @@ public class MortgageCalculator {
     public static final int MONTHS_IN_YEAR = 12;
 
     public static void main(String[] args) {
-        
-        int principal=0;
-        float annualInterest=0;
-        byte period=0;
+
+        int principal = 0;
+        float annualInterest = 0;
+        byte period = 0;
 
         principal = (int) readNumber("Principal ($1k - $1M): ", 1000, 1000_000);
-        annualInterest = (int) readNumber("Annual Interest: ", 1, 30);
+        annualInterest = (float) readNumber("Annual Interest: ", 1, 30);
         period = (byte) readNumber("Period (Years): ", 1, 30);
 
         double mortgage = calculateMortgage(principal, annualInterest, period);
 
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+
+        System.out.println("Monthly Payments : " + NumberFormat.getCurrencyInstance().format(mortgage));
+
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        printPaymentSchedule()
+
     }
 
-    public static double readNumber(String prompt,double min, double max) {
-        Scanner sc = new Scanner(System.in);
+    private static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt);
-            value = sc.nextInt();
-            if (value >= min && value <= max)
+            value = scanner.nextFloat();
+            if (value >= min && value <= max) {
                 break;
-            System.out.println("Enter Number between " + min + "and "+max);        
+            }
+            System.out.println("Enter a value between " + min + " and " + max);
         }
-        sc.close();
         return value;
     }
 
@@ -44,5 +53,25 @@ public class MortgageCalculator {
         return principal *
                 (monthlyInterest * Math.pow(1 + monthlyInterest, totalNumberOfPayments))
                 / ((Math.pow(1 + monthlyInterest, totalNumberOfPayments) - 1));
+    }
+
+    public static double calculateBalance(int principal, float annualInterest, short period, int month) {
+
+        float monthlyInterest = annualInterest / PERCENTAGE / MONTHS_IN_YEAR;
+        short totalNumberOfPayments = (short) (period * MONTHS_IN_YEAR);
+        return principal *
+                (Math.pow(1 + monthlyInterest, totalNumberOfPayments)
+                        - Math.pow(1 + monthlyInterest, month))
+                / (Math.pow(1 + monthlyInterest, totalNumberOfPayments) - 1);
+    }
+
+    public static void printPaymentSchedule(int principal, float annualInterest, short period) {
+
+        for (int numberOfPaymentsMade = 1; numberOfPaymentsMade <= period * MONTHS_IN_YEAR; numberOfPaymentsMade++) {
+
+            double remainingBalance = calculateBalance(principal, annualInterest, period, numberOfPaymentsMade);
+            System.out.println(NumberFormat.getCurrencyInstance().format(remainingBalance));
+            
+        }
     }
 }
