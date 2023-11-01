e0deceea839f571eaa2bf7a6786a1d58ab6b7bbe
prajul-shinde <prajulshinde@gmail.com>
2023-11-01 04:08:38 -0700

    finally block

---
+++ Advance/src/exceptions/FinallyDemo.java
@@ -0,0 +1,25 @@
+package exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FinallyDemo {
+
+    public static void main(String[] args) {
+        FileReader reader = null;
+        try {
+             reader= new FileReader("file1.txt");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        finally{
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+}
