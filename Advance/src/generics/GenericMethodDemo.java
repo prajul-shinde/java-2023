52e6242a1413f86fa3fa0f3c0d6c444bacbc0ff7
prajul-shinde <prajulshinde@gmail.com>
2023-11-04 03:38:07 -0700

    multiple type parameters in generic methods

--- Advance/src/generics/GenericMethodDemo.java
+++ Advance/src/generics/GenericMethodDemo.java
@@ -2,8 +2,13 @@ package generics;
 
 public class GenericMethodDemo {
 
-    public static <T extends Comparable<T>> T max(T first, T second){
-        return (first.compareTo(second)< 0) ? second: first;
+    public static <T extends Comparable<T>> T max(T first, T second) {
+        return (first.compareTo(second) < 0) ? second : first;
+    }
+
+    // mutltiple type parameters
+    public static <K, V> void print(K key, V value) {
+        System.out.println(key + " = " + value);
     }
 
     public static void main(String[] args) {
@@ -11,8 +16,10 @@ public class GenericMethodDemo {
         System.out.println(max);
 
         var max2 = GenericMethodDemo.max(new User(20), new User(15));
-        System.out.println("max user "+ max2);
-        
+        System.out.println("max user " + max2);
+
+        print(1, "Age");
+
     }
-    
+
 }
