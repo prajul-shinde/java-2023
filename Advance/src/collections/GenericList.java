c75f524d6032db6da7afd79a2f1e7e0124277015
prajul-shinde <prajulshinde@gmail.com>
2023-11-04 19:59:11 -0700

    Iterable and iterator generic interffaces

---
+++ Advance/src/collections/GenericList.java
@@ -0,0 +1,50 @@
+package collections;
+
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T> {
+
+    private T[] items = (T[]) new Object[10];
+    private int count;
+
+    private void add(T item) {
+        items[count++] = item;
+    }
+
+    private T get(int index) {
+        return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator(this);
+    }
+
+    private class ListIterator implements Iterator<T> {
+
+        private GenericList<T> list;
+        private int index;
+        
+
+        public ListIterator(GenericList<T> list) {
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
+
+    }
+    public static void main(String[] args) {
+        var list = new GenericList<String>();
+        list.add("hi");
+        list.add("prajul");
+        list.forEach(System.out::println);
+    }
+}
