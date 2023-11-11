2b6095c1b18ecf9dba6bc24b27cb7d78d94317dc
prajul-shinde <prajulshinde@gmail.com>
2023-11-11 19:35:36 +0530

    MethodReference example

---
+++ Advance/src/collections/Lambdas/MethodReferenceDemo.java
@@ -0,0 +1,24 @@
+package collections.Lambdas;
+
+public class MethodReferenceDemo {
+    
+    public static void main(String[] args) {
+        show();
+    }
+
+    public MethodReferenceDemo(String message){}
+
+    private static void show() {
+        greet(MethodReferenceDemo::print);
+        greet(MethodReferenceDemo::new);
+    }
+
+    public static void print(String message){
+        System.out.println("print method");
+    }
+
+    private static void greet(Printer printer) {
+        System.out.println("greet method");
+        printer.print("Hello World");
+    }
+}
