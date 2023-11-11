6af380b95b6653251c815039ab0580450c831892
prajul-shinde <prajulshinde@gmail.com>
2023-11-11 21:23:58 +0530

    function interface demo

--- Advance/src/Lambdas/FunctionInterfaceDemo.java
+++ Advance/src/Lambdas/FunctionInterfaceDemo.java
@@ -1,27 +1,11 @@
 package Lambdas;
 
+import java.util.function.Function;
+
 public class FunctionInterfaceDemo {
-    public static void showUsingAnonymousInnerClass() {
-        greet(new Printer() {
-
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-
-        });
-    }
-
-    public static void showUsingLambda() {
-        greet(message -> System.out.println(message));
-    }
-
-    private static void greet(Printer printer) {
-        printer.print("hello world");
-    }
-
     public static void main(String[] args) {
-        showUsingAnonymousInnerClass();
-        showUsingLambda();
+        Function<String, Integer> map = str -> str.length();
+        Integer length = map.apply("rohit_sharma");
+        System.out.println(length);
     }
 }
---
+++ Advance/src/Lambdas/FunctionalInterfaceDemo.java
@@ -0,0 +1,27 @@
+package Lambdas;
+
+public class FunctionalInterfaceDemo {
+    public static void showUsingAnonymousInnerClass() {
+        greet(new Printer() {
+
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+
+        });
+    }
+
+    public static void showUsingLambda() {
+        greet(message -> System.out.println(message));
+    }
+
+    private static void greet(Printer printer) {
+        printer.print("hello world");
+    }
+
+    public static void main(String[] args) {
+        showUsingAnonymousInnerClass();
+        showUsingLambda();
+    }
+}
