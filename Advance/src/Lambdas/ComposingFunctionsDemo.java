2b5a32efb6c916838705fb6ed32c3eee3a308f1f
prajul-shinde <prajulshinde@gmail.com>
2023-11-11 21:44:45 +0530

    composing functions

---
+++ Advance/src/Lambdas/ComposingFunctionsDemo.java
@@ -0,0 +1,14 @@
+package Lambdas;
+
+import java.util.function.Function;
+
+public class ComposingFunctionsDemo {
+    public static void main(String[] args) {
+        Function<String ,String> replaceFunction = str -> str.replace(":", "=");
+        Function<String, String> addBraces = str ->  "{" + str + "}";
+
+        String result = replaceFunction.andThen(addBraces).apply("key:value");
+
+        System.out.println(result);
+    }
+}
