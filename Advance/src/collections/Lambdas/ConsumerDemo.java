4b0325f58951ce1db7c54b1b74a809e511391a71
prajul-shinde <prajulshinde@gmail.com>
2023-11-11 19:47:38 +0530

    chaining of consumers example

--- Advance/src/collections/Lambdas/ConsumerDemo.java
+++ Advance/src/collections/Lambdas/ConsumerDemo.java
@@ -1,11 +1,20 @@
 package collections.Lambdas;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
-         List<Integer> intList = List.of(1,2,3);
+        List<Integer> intList = List.of(1, 2, 3);
         intList.forEach(System.out::println);
+
+        // chaining consumer
+        List<String> list = List.of("a", "b", "c");
+
+        Consumer<String> print = System.out::println;
+        Consumer<String> printUppercase = item -> System.out.println(item.toUpperCase());
+
+        list.forEach(print.andThen(printUppercase));
     }
-    
+
 }
