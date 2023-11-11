6d931ae1d9bd823b02df6fb900c539844b32b36d
prajul-shinde <prajulshinde@gmail.com>
2023-11-11 18:11:58 +0530

    lambda expression for printer interface

--- Advance/src/collections/Lambdas/FunctionInterfaceDemo.java
+++ Advance/src/collections/Lambdas/FunctionInterfaceDemo.java
@@ -1,22 +1,27 @@
 package collections.Lambdas;
 
 public class FunctionInterfaceDemo {
-   public static void show(){
-    greet (new Printer() {
+    public static void showUsingAnonymousInnerClass() {
+        greet(new Printer() {
 
-        @Override
-        public void print(String message) {
-            System.out.println(message);
-        }
-        
-    });
-   }
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+
+        });
+    }
+
+    public static void showUsingLambda() {
+        greet(message -> System.out.println(message));
+    }
 
     private static void greet(Printer printer) {
         printer.print("hello world");
     }
 
     public static void main(String[] args) {
-        show();
+        showUsingAnonymousInnerClass();
+        showUsingLambda();
     }
 }
