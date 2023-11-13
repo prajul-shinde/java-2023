1c994456b6e60ebe3169c1e5cb9181dce87c9dc1
prajul-shinde <prajulshinde@gmail.com>
2023-11-13 17:22:19 +0530

    reducers example

--- Advance/src/streams/ReducersDemo.java
+++ Advance/src/streams/ReducersDemo.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public class ReducersDemo {
     public static void main(String[] args) {
-        List.of(
+        List<Movies> movies = List.of(
             new Movies("a", 10),
             new Movies("b", 20),
             new Movies("c", 30)
         );
+        
+        boolean anyMatch = movies.stream().anyMatch(m -> m.getLikes() >20);
+        Movies movies2 = movies.stream().findFirst().get();
+        
     }
 }
