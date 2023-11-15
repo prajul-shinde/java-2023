7cb8b99c42e433c31409327a00179794fd40074a
prajul-shinde <prajulshinde@gmail.com>
2023-11-15 13:35:24 +0530

    synchronizedCollection demo

---
+++ Advance/src/Concurrency/ThreadSafeStrategies/SynchronizedCollection/SynchronizedCollectionDemo.java
@@ -0,0 +1,31 @@
+package Concurrency.ThreadSafeStrategies.SynchronizedCollection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SynchronizedCollectionDemo {
+    public static void main(String[] args) {
+        Collection<Integer> collection = Collections.synchronizedCollection(new ArrayList<>());
+
+        var thread1 = new Thread(() -> {
+            collection.addAll(Arrays.asList(1, 2, 3));
+        });
+        var thread2 = new Thread(() -> {
+            collection.addAll(Arrays.asList(4, 5, 6));
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(collection);
+    }
+}
