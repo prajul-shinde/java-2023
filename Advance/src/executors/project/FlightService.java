7ecf2ec66a58994cd0c6e674772b3f4f5e420077
prajul-shinde <prajulshinde@gmail.com>
2023-11-25 16:38:08 +0000

    removed unused import

--- Advance/src/executors/project/FlightService.java
+++ Advance/src/executors/project/FlightService.java
@@ -3,7 +3,6 @@ package executors.project;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import executors.LongTask;
